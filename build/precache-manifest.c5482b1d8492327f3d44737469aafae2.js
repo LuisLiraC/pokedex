e893928874adcc9396d84f663d7ecf1088430923
LuisLiraC <luis_ssbm@hotmail.com>
2019-05-23 21:11:31 -0500

    gh-pages

---
+++ build/precache-manifest.c5482b1d8492327f3d44737469aafae2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3fa9524b313090737c95b04cebea7c6",
+    "url": "/pokedex/static/media/error.f3fa9524.png"
+  },
+  {
+    "revision": "be4e755b363a2075abf6",
+    "url": "/pokedex/static/css/main.618a1664.chunk.css"
+  },
+  {
+    "revision": "c92f8f4bce56a57764ce",
+    "url": "/pokedex/static/js/runtime~main.c92f8f4b.js"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/pokedex/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "a4c5634fd6c77ab838fa",
+    "url": "/pokedex/static/js/2.a4c5634f.chunk.js"
+  },
+  {
+    "revision": "24c0b6c61d0241a0f9bf1b0cdb24ebfe",
+    "url": "/pokedex/static/media/pokeLogo2.24c0b6c6.png"
+  },
+  {
+    "revision": "6689dc331be27e66349ce9a4d15ddff3",
+    "url": "/pokedex/static/media/loading.6689dc33.gif"
+  },
+  {
+    "revision": "be4e755b363a2075abf6",
+    "url": "/pokedex/static/js/main.be4e755b.chunk.js"
+  },
+  {
+    "revision": "2724690402f7665826cc6e35f45dc9cb",
+    "url": "/pokedex/static/media/pikachu_running.27246904.gif"
+  },
+  {
+    "revision": "3cfd5e85cd2597eb00d7f5b0139d3b6d",
+    "url": "/pokedex/static/media/Pokemon_logo.3cfd5e85.svg"
+  },
+  {
+    "revision": "e64510f17468324e2e04b82a7d3d3823",
+    "url": "/pokedex/static/media/404Page.e64510f1.png"
+  },
+  {
+    "revision": "82b1d7c004912b1dae2810dbe88cc21c",
+    "url": "/pokedex/static/media/pokeLogo@1x.82b1d7c0.png"
+  },
+  {
+    "revision": "a4c5634fd6c77ab838fa",
+    "url": "/pokedex/static/css/2.de2516d6.chunk.css"
+  },
+  {
+    "revision": "431b10e9cb6cb8dca098dfdae5b8fa5c",
+    "url": "/pokedex/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f596af102bdbb2b565ba80c5ea6ac971.js"
+  "/pokedex/precache-manifest.c5482b1d8492327f3d44737469aafae2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/pokedex/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
